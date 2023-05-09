36daaa8780ddb59441cd43232ef982f8c8143a3a
jossemii <josemi.bnf@gmail.com>
2023-05-09 12:59:23 +0200

    ...

--- src/main/scala/cats.scala
+++ src/main/scala/cats.scala
@@ -1,8 +1,42 @@
-case class Cat(name: String, color: String, food: String)
 
-val first_cat: Cat = Cat(name = "Oswald", color = "Black", food = "Milk")
-val second_cat: Cat = Cat(name = "Henderson", color = "Ginger", food = "Chips")
-val third_cat: Cat = Cat(name = "Quentin", color = "Tabby and white", food = "Curry")
+/**
+
+Demand for Cat Simulator 1.0 is exploding! For v2 we’re going to go beyond
+the domes c cat to model Tigers, Lions, and Panthers in addi on to the Cat.
+Deﬁne a trait Feline and then deﬁne all the diﬀerent species as subtypes of
+Feline. To make things interes ng, deﬁne:
+• on Feline a colour as before;
+• on Feline a String sound, which for a cat is "meow" and is "roar"
+for all other felines;
+• only Cat has a favourite food; an
+
+ */
+
+trait Callable {
+  def name: String
+}
+
+trait Feline extends Callable {
+  def colour: String
+  def sound: String
+}
+
+trait BigCat extends Feline {
+  val sound = "roar"
+}
+
+case class Tiger(name: String, colour: String) extends BigCat {
+  override val sound = "roar, im a tiger ..."
+}
+case class Panther(name: String, colour: String) extends BigCat
+case class Lion(name: String, colour: String) extends BigCat
+case class Cat(name: String, colour: String, food: String) extends Feline {
+  val sound = "meow"
+}
+
+val first_cat: Cat = Cat(name = "Oswald", colour = "Black", food = "Milk")
+val second_cat: Cat = Cat(name = "Henderson", colour = "Ginger", food = "Chips")
+val third_cat: Cat = Cat(name = "Quentin", colour = "Tabby and white", food = "Curry")
 
 object ChipShop {
   def willServe(cat: Cat): Boolean = cat.food == "Chips"
