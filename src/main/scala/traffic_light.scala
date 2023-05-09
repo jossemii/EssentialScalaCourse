df880289f4ccfe2652f7554f9d9deb8d9201c240
jossemii <josemi.bnf@gmail.com>
2023-05-09 22:11:49 +0200

    ...

--- src/main/scala/traffic_light.scala
+++ src/main/scala/traffic_light.scala
@@ -1,5 +1,24 @@
-sealed trait TrafficLight
+sealed trait TrafficLight {
+  def next_oo: TrafficLight
+  def next_f: TrafficLight = this match  // Preferred solution
+    case RedLight => GreenLight
+    case GreenLight => YellowLight
+    case YellowLight => RedLight
+}
 
-case object RedLight extends TrafficLight
-case object YellowLight extends TrafficLight
-case object GreenLight extends TrafficLight
+object NextTraficLight {
+  def next(trafficLight: TrafficLight): TrafficLight = trafficLight match
+    case RedLight => GreenLight
+    case GreenLight => YellowLight
+    case YellowLight => RedLight
+}
+
+case object RedLight extends TrafficLight {
+  override def next_oo: TrafficLight = GreenLight
+}
+case object YellowLight extends TrafficLight {
+  override def next_oo: TrafficLight = RedLight
+}
+case object GreenLight extends TrafficLight {
+  override def next_oo: TrafficLight = YellowLight
+}
