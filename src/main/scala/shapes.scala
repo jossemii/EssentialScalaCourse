d96dfe71982b041609ca48683fff23ebbe2176d7
jossemii <josemi.bnf@gmail.com>
2023-05-09 16:41:11 +0200

    ...

--- src/main/scala/shapes.scala
+++ src/main/scala/shapes.scala
@@ -1,12 +1,12 @@
 import scala.math.{Pi, pow}
 
-trait Shape {
+sealed trait Shape {
   def sides: Int
   def perimeter: Double
   def area: Double
 }
 
-trait Rectangular extends Shape {
+sealed trait Rectangular extends Shape {
   def width: Double
   def height: Double
   val sides = 4
@@ -14,16 +14,32 @@ trait Rectangular extends Shape {
   def area: Double = width * height / 2
 }
 
-case class Circle(radius: Double) extends Shape
+final case class Circle(radius: Double) extends Shape
 {
   val sides = 1
   val perimeter: Double = 2 * radius * Pi
   val area: Double = pow(radius, 2) * Pi
+
+  override def toString: String = s"A circle of radius $radius cm"
 }
 
-case class Rectangle(val width: Double, val height: Double) extends Rectangular
+final case class Rectangle(width: Double, height: Double) extends Rectangular
+{
+  override def toString: String = s"A rectangle of width $width cm and height $height cm"
+}
 
-case class Square(size: Double) extends Rectangular {
+final case class Square(size: Double) extends Rectangular {
   val width: Double = size
   val height: Double = size
+
+  override def toString: String = s"A square of size $size cm"
+}
+
+object Draw {
+  def test(shape: Shape): String = shape.toString
+
+  def apply(shape: Shape): String = shape match
+    case Rectangle(width, height) => s"A rectangle of width $width cm and height $height cm"
+    case Square(size) => s"A square of size $size cm"
+    case Circle(radius) => s"A circle of radius $radius cm"
 }
