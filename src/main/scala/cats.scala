84f0a112d3d546c432bf7c43cfd5ea6216a82c32
jossemii <josemi.bnf@gmail.com>
2023-05-09 12:10:36 +0200

    ...

--- src/main/scala/cats.scala
+++ src/main/scala/cats.scala
@@ -1,17 +1,35 @@
-class Cat(val name: String, val color: String, val food: String)
+case class Cat(name: String, color: String, food: String)
 
-val first_cat = new Cat(name = "Oswald", color = "Black", food = "Milk")
-val second_cat = new Cat(name = "Henderson", color = "Ginger", food = "Chips")
-val third_cat = new Cat(name = "Quentin", color = "Tabby and white", food = "Curry")
+val first_cat: Cat = Cat(name = "Oswald", color = "Black", food = "Milk")
+val second_cat: Cat = Cat(name = "Henderson", color = "Ginger", food = "Chips")
+val third_cat: Cat = Cat(name = "Quentin", color = "Tabby and white", food = "Curry")
 
 object ChipShop {
   def willServe(cat: Cat): Boolean = cat.food == "Chips"
+  def willServeByPatternMatching(cat: Cat): Boolean = cat.food match
+    case "Chips" => true
+    case _ => false
+
+  def willServeByPatternMatching2(cat: Cat): Boolean = cat match
+    case Cat(_, _, "Chips") => true
+    case Cat(_, _, _) => false
 }
 
-case class CaseCat(name: String, color: String)
 
 def cats_test(): Unit = {
   println(ChipShop.willServe(first_cat))
   println(ChipShop.willServe(second_cat))
   println(ChipShop.willServe(third_cat))
+
+  println("------")
+
+  println(ChipShop.willServeByPatternMatching(first_cat))
+  println(ChipShop.willServeByPatternMatching(second_cat))
+  println(ChipShop.willServeByPatternMatching(third_cat))
+
+  println("------")
+
+  println(ChipShop.willServeByPatternMatching2(first_cat))
+  println(ChipShop.willServeByPatternMatching2(second_cat))
+  println(ChipShop.willServeByPatternMatching2(third_cat))
 }
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,13 +1,4 @@
-object calc {
-  def square(number: Double): Double = number * number
-  def cube(number: Double): Double = number * square(number)
-
-  def square(number: Int): Int = number * number
-
-  def cube(number: Int): Int = number * square(number)
-}
-
 @main
 def main(): Unit = {
-  case_director_test()
+  cats_test()
 }
