f96d5e5ef7cb5f66506535482d7664a10aad6904
jossemii <josemi.bnf@gmail.com>
2023-05-14 17:43:01 +0200

    map.

--- src/main/scala/map.scala
+++ src/main/scala/map.scala
@@ -1,9 +1,9 @@
 sealed trait LinkedList[A] {
   def map[B](fn: A => B): LinkedList[B] =
     this match {
-      case Pair(hd, tl) => Pair(fn(hd), tl.map(fn))
+      case Pair_map(hd, tl) => Pair_map(fn(hd), tl.map(fn))
       case End() => End[B]()
     }
 }
-final case class Pair[A](head: A, tail: LinkedList[A]) extends LinkedList[A]
+final case class Pair_map[A](head: A, tail: LinkedList[A]) extends LinkedList[A]
 final case class End[A]() extends LinkedList[A]
