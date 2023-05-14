58aaec0a4f83e69ff2055d9643a1b6ff091b3aa9
jossemii <josemi.bnf@gmail.com>
2023-05-14 19:00:07 +0200

    maybe variance

--- src/main/scala/maybe.scala
+++ src/main/scala/maybe.scala
@@ -1,8 +1,10 @@
-sealed trait Maybe_[A] {
-  def fold[B](full: A => B, empty: B): B = this match
-    case Empty_() => empty
-    case Full_(value) => full(value)
-}
+object maybe{
+  sealed trait Maybe_[+A] {
+    def fold[B](full: A => B, empty: B): B = this match
+      case Empty_ => empty
+      case Full_(value) => full(value)
+  }
 
-final case class Empty_[A]() extends Maybe_[A]
-final case class Full_[A](value: A) extends Maybe_[A]
+  case object Empty_ extends Maybe_[Nothing]
+  final case class Full_[A](value: A) extends Maybe_[A]
+}
--- src/main/scala/sum_fold.scala
+++ src/main/scala/sum_fold.scala
@@ -1,15 +1,19 @@
-sealed trait Sum[A, B] {
-  def fold[C](error: A => C, success: B => C): C = this match
-    case Failure(a) => error(a)
-    case Success(b) => success(b)
+object sum_fold {
+  sealed trait Sum[A, B] {
+    def fold[C](error: A => C, success: B => C): C = this match
+      case Failure(a) => error(a)
+      case Success(b) => success(b)
 
-  def map[C](f: B => C): Sum[A, C] = this match
-    case Failure(value) => Failure(value)
-    case Success(value) => Success(f(value))
+    def map[C](f: B => C): Sum[A, C] = this match
+      case Failure(value) => Failure(value)
+      case Success(value) => Success(f(value))
 
-  def flatMap[C](f: B => Sum[A, C]): Sum[A, C] = this match
-    case Failure(value) => Failure(value)
-    case Success(value) => f(value)
+    def flatMap[C](f: B => Sum[A, C]): Sum[A, C] = this match
+      case Failure(value) => Failure(value)
+      case Success(value) => f(value)
+  }
+
+  final case class Failure[A, B](value: A) extends Sum[A, B]
+
+  final case class Success[A, B](value: B) extends Sum[A, B]
 }
-final case class Failure[A, B](value: A) extends Sum[A, B]
-final case class Success[A, B](value: B) extends Sum[A, B]
